2d41dd79669ee95690f2a0e0847458c4e1723057
alextitonis <alexandrostitonis@gmail.com>
2023-04-22 10:20:12 +0300

    multithreading

--- imgServer.py
+++ imgServer.py
@@ -1,25 +1,51 @@
+import threading
 import cv2
 import socket
 import pickle
-import numpy as np
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ip = "127.0.0.1"
-port = 7777
-s.bind((ip, port))
+running = True
+client = None
 
-while True:
-    x = s.recvfrom(1000000)
-    clientip = x[1][0]
-    data = x[0]
+def on_new_client(clientsocket,addr):
+    global client, running
+    client = clientsocket
 
-    data = pickle.loads(data)
+    print("Connected to: " + str(addr))
+    while running:
+        x = clientsocket.recvfrom(1000000)
+        clientip = x[1][0]
+        data = x[0]
 
-    img = cv2.imdecode(data, cv2.IMREAD_COLOR)
-    
-    cv2.imshow('Img Server', img)
-    
-    if cv2.waitKey(5) & 0xFF == 27:
-        break
-    
-cv2.destroyAllWindows()
+        data = pickle.loads(data)
+
+        img = cv2.imdecode(data, cv2.IMREAD_COLOR)
+        
+        cv2.imshow('Img Server', img)
+        
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
+    cv2.destroyAllWindows()
+    clientsocket.close()
+
+def closeClient():
+    global running, client
+    running = False
+
+def send(msg):
+    global client
+    client.send(msg)
+
+def initImgServer():
+    s = socket.socket()
+    ip = "127.0.0.1"
+    port = 7777
+    s.bind((ip, port))
+    s.listen(1)
+    print("listening")
+
+    while True:
+        c, addr = s.accept()   
+        threading.Thread(target=on_new_client, args=(c,addr)).start()
+        
+    s.close()
